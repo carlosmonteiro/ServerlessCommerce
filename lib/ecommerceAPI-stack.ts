88d8c0fa2483fd0764d4da425440ef8da862ef07
Carlos Monteiro <carlosmonteiro@msn.com>
2024-08-26 19:34:14 -0300

    Create the API gateway endpoint /products

---
+++ lib/ecommerceAPI-stack.ts
@@ -0,0 +1,42 @@
+import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs"
+import * as cdk from "aws-cdk-lib"
+import * as apigateway from "aws-cdk-lib/aws-apigateway"
+import * as cwlogs from "aws-cdk-lib/aws-logs"
+import { Construct } from "constructs"
+
+interface EcommerceApiStackProps extends cdk.StackProps{
+    productsFetchHandler: lambdaNodeJS.NodejsFunction
+}
+
+export class EcommerceApiStack extends cdk.Stack{
+
+    constructor(scope: Construct, id: string, props: EcommerceApiStackProps){
+        super(scope, id, props)
+
+        //Create Cloudwatch log group
+        const logGroup = new cwlogs.LogGroup(this, "ECommerceAPILogs")
+
+        const api = new apigateway.RestApi(this, "EcommerceApi", {
+            restApiName: "EcommerceAPI",
+            cloudWatchRole: true,
+            deployOptions: {
+                accessLogDestination: new apigateway.LogGroupLogDestination(logGroup),
+                accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields({
+                    httpMethod: true, 
+                    ip: true,
+                    protocol: true,
+                    requestTime: true,
+                    resourcePath: true,
+                    responseLength: true,
+                    status: true, 
+                    caller: true,
+                    user: true
+                })
+            }
+        })
+
+        const productsFetchIntegration = new apigateway.LambdaIntegration(props.productsFetchHandler)
+        const productsResource = api.root.addResource("products")
+        productsResource.addMethod("GET", productsFetchIntegration)
+    }
+}
