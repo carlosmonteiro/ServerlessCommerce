3763ad22c17fcfc8c2a511b2d45f7b5dc4f6bd3f
Carlos Monteiro <carlosmonteiro@msn.com>
2024-09-12 17:29:11 -0300

    Create Product Table on Dynamo and the CRUD APIs

--- lib/ecommerceAPI-stack.ts
+++ lib/ecommerceAPI-stack.ts
@@ -6,6 +6,7 @@ import { Construct } from "constructs"
 
 interface EcommerceApiStackProps extends cdk.StackProps{
     productsFetchHandler: lambdaNodeJS.NodejsFunction
+    productsAdminHandler: lambdaNodeJS.NodejsFunction
 }
 
 export class EcommerceApiStack extends cdk.Stack{
@@ -36,7 +37,24 @@ export class EcommerceApiStack extends cdk.Stack{
         })
 
         const productsFetchIntegration = new apigateway.LambdaIntegration(props.productsFetchHandler)
+        
+        //GET /products
         const productsResource = api.root.addResource("products")
         productsResource.addMethod("GET", productsFetchIntegration)
+
+        //GET /products/{id}
+        const productIdResource = productsResource.addResource("{id}")
+        productIdResource.addMethod("GET", productsFetchIntegration)
+
+        const productsAdminIntegration = new apigateway.LambdaIntegration(props.productsAdminHandler)
+
+        //POST /products
+        productsResource.addMethod("POST", productsAdminIntegration)
+
+        //PUT /products/{id}
+        productIdResource.addMethod("PUT", productsAdminIntegration)
+
+        //DELETE /products/{id}
+        productIdResource.addMethod("DELETE", productsAdminIntegration)
     }
 }
--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -1,17 +1,31 @@
 import * as lambda from "aws-cdk-lib/aws-lambda"
 import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs"
 import * as cdk from "aws-cdk-lib"
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb"
 import { Construct } from "constructs"
 
 export class ProductsAppStack extends cdk.Stack{
     
     readonly productsFetchHandler: lambdaNodeJS.NodejsFunction
+    readonly productsAdminHandler: lambdaNodeJS.NodejsFunction
+    readonly productsDdb: dynamodb.Table
 
     constructor(scope: Construct, id: string, props?: cdk.StackProps){
         super(scope, id, props)
 
+        this.productsDdb = new dynamodb.Table(this, "ProductsDdb", {
+            tableName: "products",
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+            partitionKey: {
+                name: "id",
+                type: dynamodb.AttributeType.STRING
+            },
+            billingMode: dynamodb.BillingMode.PROVISIONED,
+            readCapacity: 1,
+            writeCapacity: 1
+        })
+
         this.productsFetchHandler = new lambdaNodeJS.NodejsFunction(this, "ProductsFetchFunction", {
-           
             functionName: "ProductsFetchFunction",
             entry: "lambda/products/productsFetchFunction.ts",
             handler: "handler",
@@ -21,7 +35,30 @@ export class ProductsAppStack extends cdk.Stack{
             bundling: {
                 minify: true,
                 sourceMap: false
+            },
+            environment: {
+                PRODUCTS_DDB: this.productsDdb.tableName
             }
         })
+        //Allow read permissions to access DynamoDB Table
+        this.productsDdb.grantReadData(this.productsFetchHandler)
+
+        this.productsAdminHandler = new lambdaNodeJS.NodejsFunction(this, "ProductsAdminFunction", {
+            functionName: "ProductsAdminFunction",
+            entry: "lambda/products/productsAdminFunction.ts",
+            handler: "handler",
+            runtime: lambda.Runtime.NODEJS_20_X,
+            memorySize: 512,
+            timeout: cdk.Duration.seconds(5),
+            bundling: {
+                minify: true,
+                sourceMap: false
+            },
+            environment: {
+                PRODUCTS_DDB: this.productsDdb.tableName
+            }            
+        })
+        //Allow write permissions to access DynamoDB Table
+        this.productsDdb.grantReadWriteData(this.productsAdminHandler)
     }
 }
